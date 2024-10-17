50d64e34e5464f9ee667b030c1951a08e8806dae
Paulo <paul.brto@gmail.com>
2024-10-17 19:32:11 -0300

    feat: database population

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const {
     geradorNome,
     geradorNomeFeminino,
     geradorNomeMasculino
-  } = require("gerador-nome");
+} = require("gerador-nome");
+
+const alunos = require('./jsons/aluno.json');
+const professores = require('./jsons/professor.json');
+const matrizCurricular = require('./jsons/matriz_curricular.json');
 
 const {
     Professor,
@@ -26,95 +30,45 @@ const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) +
 const getRandomNota = () => parseFloat((Math.random() * 10).toFixed(1));
 
 const main = async () => {
-    const matrizCurricular1 = await MatrizCurricular.create({
-        curso: {
-            id: 1,
-            nome: 'Engenharia da Computação',
-        },
-        disciplinas: [
-            { nome: 'Calculo I', codigo: 'MAT101' },
-            { nome: 'Física I', codigo: 'FIS101' },
-            { nome: 'Introdução à Computação', codigo: 'COMP101' },
-            { nome: 'Banco de Dados I', codigo: 'BD101' },
-            { nome: 'Programação Orientada a Objetos', codigo: 'POO101' },
-            { nome: 'Automatização e Robótica', codigo: 'AR101' }
-        ],
-    });
+    try {
+        // Check if the Aluno collection is empty
+        const alunoCount = await Aluno.find()
+        console.log(alunoCount.length);
+        if (alunoCount.length === 0) {
+            Object.keys(alunos).forEach(async (key) => {
+                const aluno = alunos[key];
+                await Aluno.create(aluno)
+              });
+        }
+            
+         
 
-    const matrizCurricular2 = await MatrizCurricular.create({
-        curso: {
-            id: 2,
-            nome: 'Ciência da Computação',
-        },
-        disciplinas: [
-            { nome: 'Calculo I', codigo: 'MAT101' },
-            { nome: 'Física I', codigo: 'FIS101' },
-            { nome: 'Introdução à Computação', codigo: 'COMP101' },
-            { nome: 'Banco de Dados I', codigo: 'BD101' },
-            { nome: 'Programação Orientada a Objetos', codigo: 'POO101' },
-            { nome: 'Estrutura de Dados', codigo: 'ED101' },
-            { nome: 'Inteligência Artificial', codigo: 'IA101' }
-        ],
-    });
+        // Check if the Professor collection is empty
+        const professorCount = await Professor.find()
+        if (professorCount.length === 0) {
+            Object.keys(professores).forEach(async (key) => {
+                const professor = professores[key];
+                await Professor.create(professor)
+              });
+        }
 
-    // Criar professores com disciplinas aleatórias
-    const professor = await Professor.create({
-        nome: geradorNome(),
-        departamento: 'Departamento de Ciência da Computação',
-        isChefe: true,
-        disciplinas: matrizCurricular2.disciplinas.map(disciplina => ({
-            nome: disciplina.nome,
-            codigo: disciplina.codigo,
-            semestre: getRandomInt(1, 2),
-            ano: getRandomInt(2020, 2024)
-        }))
-    });
+        // Check if the MatrizCurricular collection is empty
+        const matrizCurricularCount = await MatrizCurricular.find()
+        if (matrizCurricularCount.length === 0) {
+            Object.keys(matrizCurricular).forEach(async (key) => {
+                const matrizCurriculars = matrizCurricular[key];
+                await MatrizCurricular.create(matrizCurriculars)
+                
+              });
+        }
+
+        console.log('Database initialization complete.');
+
+    } catch (error) {
+        console.error('Error initializing database:', error);
+    } 
 
     
-    const gerarHistorico = (disciplinas) => {
-        return disciplinas.map(disciplina => {
-            const nota = getRandomNota();
-            return {
-                disciplina: disciplina.nome,
-                codigo: disciplina.codigo,
-                semestre: getRandomInt(1, 2),
-                ano: getRandomInt(2020, 2024),
-                nota: nota
-            };
-        });
-    };
-
-    
-    for (let i = 0; i < 10; i++) {
-        const cursoId = getRandomInt(1, 2);
-        const curso = cursoId === 1 ? matrizCurricular1 : matrizCurricular2;
-        
-        // Criar um histórico de disciplinas para o aluno
-        const historico = gerarHistorico(curso.disciplinas);
-
-        // Verifica se o aluno passou (notas >= 6)
-
-        await Aluno.create({
-            nome: geradorNome(),
-            RA: 'RA' + Math.floor(Math.random() * 100000),
-            curso: {
-                id: cursoId,
-                nome: curso.curso.nome
-            },
-            historico: historico,
-            tcc: {
-                id: getRandomInt(1, 10),
-                nome: 'Projeto Final - ' + curso.curso.nome,
-                orientador: {
-                    id: professor._id,
-                    nome: professor.nome
-                }
-            },
-            semestre: getRandomInt(1, 8)
-        });
-    }
-
-    console.log('Dados inseridos com sucesso!');
-};
+}
 
 main();
--- schema.js
+++ schema.js
@@ -20,17 +20,6 @@ const professorSchema = new mongoose.Schema({
   ]
 });
 
-// const cursoSchema = new mongoose.Schema({
-//   nome: { type: String, required: true },
-//   departamento_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Departamento', required: true }
-// });
-
-// const disciplinaSchema = new mongoose.Schema({
-//   codigo: { type: String, required: true },
-//   nome: { type: String, required: true },
-//   departamento_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Departamento', required: true },
-//   professor_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Professor', required: true }
-//});
 
 const matrizCurricularSchema = new mongoose.Schema({
   curso:{
@@ -69,44 +58,13 @@ const alunoSchema = new mongoose.Schema({
   semestre: { type: Number, required: true }
 });
 
-// const historicoAlunoSchema = new mongoose.Schema({
-//   aluno_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Aluno', required: true },
-//   disciplina_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Disciplina', required: true },
-//   semestre: { type: Number, required: true },
-//   ano: { type: Number, required: true },
-//   nota: { type: Number, required: true }
-// });
-
-// const tccSchema = new mongoose.Schema({
-//   nome: { type: String, required: true },
-//   orientador_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Professor', required: true }
-// });
-
-// const alunoTccSchema = new mongoose.Schema({
-//   aluno_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Aluno', required: true },
-//   tcc_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Tcc', required: true }
-// });
-
-//const Departamento = mongoose.model('Departamento', departamentoSchema);
 const Professor = mongoose.model('Professor', professorSchema);
-//const ChefeDepartamento = mongoose.model('ChefeDepartamento', chefeDepartamentoSchema);
-//const Curso = mongoose.model('Curso', cursoSchema);
-//const Disciplina = mongoose.model('Disciplina', disciplinaSchema);
 const MatrizCurricular = mongoose.model('MatrizCurricular', matrizCurricularSchema);
 const Aluno = mongoose.model('Aluno', alunoSchema);
-//const HistoricoAluno = mongoose.model('HistoricoAluno', historicoAlunoSchema);
-//const Tcc = mongoose.model('Tcc', tccSchema);
-//const AlunoTcc = mongoose.model('AlunoTcc', alunoTccSchema);
+
 
 module.exports = {
-  //Departamento,
   Professor,
-  //ChefeDepartamento,
-  //Curso,
-  //Disciplina,
   MatrizCurricular,
   Aluno,
-  //HistoricoAluno,
-  //Tcc,
-  //AlunoTcc
 };
